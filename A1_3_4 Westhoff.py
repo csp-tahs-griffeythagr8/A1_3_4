f14aa369cd45f7b8e19fa40c9ad2a1599d07e0d7
RW3110 <RW3110@k12.sd.us>
2019-11-18 08:36:11 -0600

    Update A1_3_4 Westhoff.py

--- A1_3_4 Westhoff.py
+++ A1_3_4 Westhoff.py
@@ -6,6 +6,18 @@ Programmer: Reece Westhoff
 #Place Your Code Below
 #I will read the repository ReadMe
 #I will update document with my own code
+import random
+guess = 26
+def hiLo_game(guess):
+    rand_num = random.randint(1,100)
+    if rand_num > guess:
+        print ('Too low')
+    if rand_num < guess:
+        print ('Too high')
+    if rand_num == guess:
+        print ('Winner')
+hiLo_game(guess)
+
 usr_input = input()
 try:
   sani_input = int(usr_input)
