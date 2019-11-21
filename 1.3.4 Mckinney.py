eca404e33c89959345055ed4835d81fd412c3890
cm3365 <57448218+cm3365@users.noreply.github.com>
2019-11-21 08:19:53 -0600

    Update 1.3.4 Mckinney.py
    created a hilo game

--- 1.3.4 Mckinney.py
+++ 1.3.4 Mckinney.py
@@ -4,9 +4,9 @@ Connor Mckinney
 '''
 import random
 guess= 47
-def hiLo_game(guess):
+def hiLo_game(usr_input):
     rand_num = 788
-    if guess == rand_num:
+    if usr_input == rand_num:
         print ("Congrats you got it right")
     elif guess > rand_num:
         print ("your number is too high")
